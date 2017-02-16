396dd3b39dda0a6eae37ddc2dda1506c9394c4fa
cibernox <miguel.camba@gmail.com>
2017-02-16 17:59:08 +0000

    Initial commit

---
+++ addon-test-support/helpers.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+const { run, $ } = Ember;
+
+// integration helpers
+function focus(el) {
+  if (!el) { return; }
+  let $el = $(el);
+  if ($el.is(':input, [contenteditable=true]')) {
+    let type = $el.prop('type');
+    if (type !== 'checkbox' && type !== 'radio' && type !== 'hidden') {
+      run(null, function() {
+        // Firefox does not trigger the `focusin` event if the window
+        // does not have focus. If the document doesn't have focus just
+        // use trigger('focusin') instead.
+
+        if (!document.hasFocus || document.hasFocus()) {
+          el.focus();
+        } else {
+          $el.trigger('focusin');
+        }
+      });
+    }
+  }
+}
+
+export function click(selector, options = {}) {
+  let mousedown = new window.Event('mousedown', { bubbles: true, cancelable: true, view: window });
+  let mouseup = new window.Event('mouseup', { bubbles: true, cancelable: true, view: window });
+  let click = new window.Event('click', { bubbles: true, cancelable: true, view: window });
+  Object.keys(options).forEach(key => {
+    mousedown[key] = options[key];
+    mouseup[key] = options[key];
+    click[key] = options[key];
+  });
+  let element = document.querySelector(selector);
+  run(() => element.dispatchEvent(mousedown));
+  focus(element);
+  run(() => element.dispatchEvent(mouseup));
+  run(() => element.dispatchEvent(click));
+}
---
+++ tests/integration/click-test.js
@@ -0,0 +1,42 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { click } from 'ember-native-dom-helpers/test-support/helpers';
+
+
+moduleForComponent('click', 'Integration | Test Helper | click', {
+  integration: true
+});
+
+test('It fires mousedown, focus, mouseup and click events on the element with the given selector (in that order)', function(assert) {
+  assert.expect(12);
+  let index = 0;
+  this.onMouseDown = (e) => {
+    assert.equal(++index, 1, 'mousedown is fired first');
+    assert.ok(true, 'a mousedown event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+  }
+  this.onFocus = (e) => {
+    assert.equal(++index, 2, 'focus is fired second');
+    assert.ok(true, 'a focus event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+  }
+  this.onMouseUp = (e) => {
+    assert.equal(++index, 3, 'mouseup is fired third');
+    assert.ok(true, 'a mouseup event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+  }
+  this.onClick = (e) => {
+    assert.equal(++index, 4, 'click is fired third');
+    assert.ok(true, 'a click event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+  }
+
+  this.render(hbs`
+    <input class="target-element"
+      onmouseup={{onMouseUp}}
+      onfocus={{onFocus}}
+      onclick={{onClick}}
+      onmousedown={{onMouseDown}} />
+  `);
+  click('.target-element');
+});
