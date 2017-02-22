4c684524f0c746d23cbb5c79eadfc21411bb51b7
Bill Heaton <pixelhandler@gmail.com>
2017-02-22 09:26:14 -0800

    Fire focus event even when window is not in focus
    - When running tests window may not be in focus, so manually
      fire focus, otherwise use el.focus()
    - Remove $ (jQuery) from helper
    - Use a real browsers instead of PhantomJS

--- testem.js
+++ testem.js
@@ -3,10 +3,12 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "PhantomJS",
+    "Firefox",
+    "Chrome"
   ],
   "launch_in_dev": [
-    "PhantomJS",
+    "Firefox",
     "Chrome"
   ]
 };
