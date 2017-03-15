2491bf05a1460baab351417535b079788b5807af
Bill Heaton <pixelhandler@gmail.com>
2017-03-14 21:17:08 -0700

    Add integration test for focus event helper

---
+++ tests/integration/focus-test.js
@@ -0,0 +1,45 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { focus } from 'ember-native-dom-helpers/test-support/focus';
+
+moduleForComponent('focus', 'Integration | Test Helper | focus', {
+  integration: true
+});
+
+test('It accepts an HTMLElement as first argument', function(assert) {
+  assert.expect(2);
+  this.onFocus = (e) => {
+    assert.ok(true, 'a focus event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+  };
+
+  this.render(hbs`<input class="target-element" onfocus={{onFocus}} />`);
+  focus(document.querySelector('.target-element'));
+});
+
+test('It blurs the focused input before firing focus event on another one', function(assert) {
+  assert.expect(8);
+  let index = 0;
+  this.onBlur = (e) => {
+    assert.equal(++index, 1, 'blur is fired first');
+    assert.ok(true, 'a blur event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+    assert.equal(e.target.value, 'a');
+  };
+  this.onFocus = (e) => {
+    assert.equal(++index, 2, 'focus is fired first');
+    assert.ok(true, 'a focus event is fired');
+    assert.ok(e instanceof window.Event, 'It receives a native event');
+    assert.equal(e.target.value, 'b');
+  };
+
+  this.render(hbs`
+    <input class="other-element" onblur={{onBlur}} value="a" />
+    <input class="target-element" onfocus={{onFocus}} value="b" />
+  `);
+  let el = document.querySelector('.other-element');
+  focus(el);
+
+  el = document.querySelector('.target-element');
+  focus(el);
+});
