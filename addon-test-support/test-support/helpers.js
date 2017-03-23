d13abe566df68d89155a9b9ea779d73139c4689c
Robert Jackson <rjackson@linkedin.com>
2017-03-23 16:21:19 -0400

    Add deprecation for folks importing from prior location.

--- addon-test-support/test-support/helpers.js
+++ addon-test-support/test-support/helpers.js
@@ -1,3 +1,11 @@
+import Ember from 'ember';
+
+Ember.deprecate(
+  `Update imports for ember-native-dom-helpers to use:\n \`import { click, fillIn } from 'ember-native-dom-helpers';`,
+  false,
+  { id: 'ember-native-dom-helpers.test-support.helpers', until: '0.4.0' }
+);
+
 export { find } from '../find';
 export { findAll } from '../find-all';
 export { findWithAssert } from '../find-with-assert';
