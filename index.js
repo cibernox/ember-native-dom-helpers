19adea1683fc386baca6eb7c83cd0a147bd4d586
Robert Jackson <rjackson@linkedin.com>
2017-03-23 16:16:10 -0400

    Implement treeForAddonTestSupport to override shifting behavior.
    Leads to nicer imports:
    
    ```js
    // new
    import { click, fillIn } from 'ember-native-dom-helpers';
    
    // old
    import { click, fillIn } from 'ember-native-dom-helpers/test-support/helpers';
    ```

--- index.js
+++ index.js
@@ -2,5 +2,23 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-native-dom-helpers'
+  name: 'ember-native-dom-helpers',
+
+  treeForAddonTestSupport(tree) {
+    // intentionally not calling _super here
+    // so that can have our `import`'s be
+    // import { click, fillIn } from 'ember-native-dom-helpers';
+
+    const Funnel = require('broccoli-funnel');
+
+    let namespacedTree = new Funnel(tree, {
+      srcDir: '/',
+      destDir: `/${this.moduleName()}`,
+      annotation: `Addon#treeForTestSupport (${this.name})`,
+    });
+
+    return this.preprocessJs(namespacedTree, '/', this.name, {
+      registry: this.registry,
+    });
+  }
 };
