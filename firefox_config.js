abbc768ed79498edde9a6760335af56408927c3b
cibernox <miguel.camba@gmail.com>
2017-03-23 15:31:15 +0000

    Never check for default browser
    I suspect that this is what is making the tests fail

--- firefox_config.js
+++ firefox_config.js
@@ -1,1 +1,4 @@
 user_pref("dom.w3c_touch_events.enabled", 1);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.shell.skipDefaultBrowserCheck", true);
+user_pref("browser.shell.skipDefaultBrowserCheckOnFirstRun", true);
