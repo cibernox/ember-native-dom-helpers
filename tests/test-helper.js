127fb2db1a8d312a1a9c1b6037256cc40394571d
cibernox <miguel.camba@gmail.com>
2017-04-01 16:51:43 -0700

    Update to ember-cli 2.13.0-beta.3

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,11 +2,11 @@ import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
-
-setResolver(resolver);
-
+import { start } from 'ember-cli-qunit';
 import { settings } from 'ember-native-dom-helpers';
 import config from '../config/environment';
-const { APP: { rootElement } } = config;
 
+setResolver(resolver);
+const { APP: { rootElement } } = config;
 settings.rootElement = rootElement || settings.rootElement;
+start();
