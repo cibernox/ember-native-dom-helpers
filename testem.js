86c470bda2778fe520728c7f612b9b752d3a7a2e
cibernox <miguel.camba@gmail.com>
2017-06-09 19:39:06 +0100

    Test in headless chrome

--- testem.js
+++ testem.js
@@ -1,19 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "firefox_user_js": "./firefox_config.js",
-  "launch_in_ci": [
-    // "Firefox",
-    "Chrome"
+  'test_page': 'tests/index.html?hidepassed',
+  'disable_watching': true,
+  'firefox_user_js': './firefox_config.js',
+  'launch_in_ci': [
+    // 'Firefox',
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Firefox",
-    "Chrome"
+  'launch_in_dev': [
+    'Firefox',
+    'Chrome'
   ],
-  "browser_args": {
-    "Chrome": [
-      "--touch-events"
+  'browser_args': {
+    'Chrome': [
+      '--touch-events',
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222'
     ]
   }
 };
