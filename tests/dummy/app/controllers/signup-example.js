362bdceb205b67ea1641f2b73d6b25e0c368a23c
Michael Villander <nousvillander@gmail.com>
2017-07-09 20:27:21 -0300

    Fix functions by review and improve ember syntax

--- tests/dummy/app/controllers/signup-example.js
+++ tests/dummy/app/controllers/signup-example.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 import fetch from 'fetch';
 
-export default Ember.Controller.extend({
+const { Controller, set, getProperties } = Ember;
+
+export default Controller.extend({
   actions: {
     signup() {
-      this.set('isSaving', true);
-      let data = JSON.stringify(this.getProperties('email', 'password', 'passwordConfirmation'));
+      set(this, 'isSaving', true);
+      let data = JSON.stringify(getProperties(this, 'email', 'password', 'passwordConfirmation'));
       return fetch('/signup', { method: 'POST', body: data }).then((r) => r.json())
         .then(({ data: { id } }) => {
           this.transitionToRoute('dashboard-example', id);
-        }).finally(() => this.set('isSaving', false));
+        }).finally(() => set(this, 'isSaving', false));
     }
   }
 });
