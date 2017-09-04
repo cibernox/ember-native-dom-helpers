2decf4979fbbcd1547b16938a3eb7df89d34820f
Miguel Camba <miguel.camba@gmail.com>
2017-09-05 01:21:48 +0200

    Update ember-cli and ember (#106)
    * Update ember-cli and ember
    
    * Remove firefox from now from travis

--- testem.js
+++ testem.js
@@ -1,22 +1,26 @@
 /* eslint-env node */
 module.exports = {
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'firefox_user_js': './firefox_config.js',
-  'launch_in_ci': [
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  firefox_user_js: './firefox_config.js',
+  launch_in_ci: [
     // 'Firefox',
     'Chrome'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'Firefox',
     'Chrome'
   ],
-  'browser_args': {
-    'Chrome': [
+  browser_args: {
+    Firefox: [
+      '-headless'
+    ],
+    Chrome: [
       '--touch-events',
       '--disable-gpu',
       '--headless',
-      '--remote-debugging-port=9222'
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
     ]
   }
 };
