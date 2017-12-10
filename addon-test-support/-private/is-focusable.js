2581cf5207e4f16b86af5ecb483085ef463aeff0
Miguel Camba <miguel.camba@gmail.com>
2017-12-10 15:54:46 +0100

    Use built=in `Element.isContentEditable` property (#134)
    Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/isContentEditable
    Closes #133

--- addon-test-support/-private/is-focusable.js
+++ addon-test-support/-private/is-focusable.js
@@ -1,10 +1,9 @@
 import isFormControl from './is-form-control';
-import isContentEditable from './is-content-editable';
 
 export default function isFocusable(el) {
   let focusableTags = ['LINK', 'A'];
 
-  if (isFormControl(el) || isContentEditable(el) || focusableTags.indexOf(el.tagName) > -1) {
+  if (isFormControl(el) || el.isContentEditable || focusableTags.indexOf(el.tagName) > -1) {
     return true;
   }
 
